b2bc12a95e5a5948037cd33125f3e92c1d6beb26
pleasantvik <adedayo.ayoola5683@gmail.com>
2024-04-11 20:09:16 +0100

    fix drawer error

--- src/shared/Drawer/interface.ts
+++ src/shared/Drawer/interface.ts
@@ -1,38 +1,38 @@
-export {};
-// import React from "react";
-// import { FieldValues, UseFormReturn } from "react-hook-form";
-// // import { IButtonProps } from 'shared/Button/interface';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import React from "react";
+import { FieldValues, UseFormReturn } from "react-hook-form";
+// import { IButtonProps } from 'shared/Button/interface';
 
-// export type IModalActionProps = {
-//   show?: boolean;
-//   text?: React.ReactNode;
-//   variant?: IButtonProps["variant"];
-//   color?: IButtonProps["color"];
-//   onClick?: (_data: any) => void;
-//   disabled?: boolean;
-//   className?: string;
-//   loading?: boolean;
-//   loadingText?: React.ReactNode;
-// };
+export type IModalActionProps = {
+  show?: boolean;
+  text?: React.ReactNode;
+  variant?: any["variant"];
+  color?: any["color"];
+  onClick?: (_data: any) => void;
+  disabled?: boolean;
+  className?: string;
+  loading?: boolean;
+  loadingText?: React.ReactNode;
+};
 
-// export type IModalBaseProps = {
-//   onClose: () => void;
-//   children?: React.ReactNode;
-//   header?: React.ReactNode;
-//   className?: string;
-//   containerClassName?: string;
-// };
+export type IModalBaseProps = {
+  onClose: () => void;
+  children?: React.ReactNode;
+  header?: React.ReactNode;
+  className?: string;
+  containerClassName?: string;
+};
 
-// export type IModalProps = IModalBaseProps & {
-//   icon?: React.ReactNode;
-//   subHeader?: React.ReactNode;
-//   action?: IModalActionProps;
-//   secondaryAction?: IModalActionProps;
-// };
+export type IModalProps = IModalBaseProps & {
+  icon?: React.ReactNode;
+  subHeader?: React.ReactNode;
+  action?: IModalActionProps;
+  secondaryAction?: IModalActionProps;
+};
 
-// export type IFilterModalProps<TFieldValues extends FieldValues> =
-//   IModalBaseProps & {
-//     action?: IModalActionProps;
-//     showClearAll?: boolean;
-//     form: UseFormReturn<TFieldValues>;
-//   };
+export type IFilterModalProps<TFieldValues extends FieldValues> =
+  IModalBaseProps & {
+    action?: IModalActionProps;
+    showClearAll?: boolean;
+    form: UseFormReturn<TFieldValues>;
+  };
